30630bb2fbf4bb09020da2983d5f0ebe489691fe
kaikool <96827322+kaikool@users.noreply.github.com>
2025-09-16 20:25:22 +0700

    Delete Functions directory

--- Functions/health.js
+++
@@ -1,5 +0,0 @@
-export const onRequestGet = async () => {
-  return new Response(JSON.stringify({ ok: true, time: new Date().toISOString() }), {
-    headers: { "content-type": "application/json; charset=utf-8" },
-  });
-};
