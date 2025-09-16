c673b29cee1cd7b50d9eb76f7c1f2b86a9ad2f46
kaikool <96827322+kaikool@users.noreply.github.com>
2025-09-16 20:24:34 +0700

    Create health.js

---
+++ functions/health.js
@@ -0,0 +1,5 @@
+export const onRequestGet = async () => {
+  return new Response(JSON.stringify({ ok: true, time: new Date().toISOString() }), {
+    headers: { "content-type": "application/json; charset=utf-8" },
+  });
+};
